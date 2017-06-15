3f02ad5441dae218bac1579883dce683febf8bbe
Dirk Elmendorf <dirk@economysizegeek.com>
2017-06-14 20:24:51 -0500

    First committ of boilerplat to reserve name

---
+++ spec/wiki-wiki-wall-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import WikiWikiWallView from '../lib/wiki-wiki-wall-view';
+
+describe('WikiWikiWallView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
